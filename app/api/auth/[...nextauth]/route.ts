f60c93101e0d79b555f80a760df3e6b26173154f
sksingh2005 <shashanksgh3@gmail.com>
2025-04-18 23:18:03 +0530

    New dep

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import { NEXT_AUTH_CONFIG } from "@/lib/auth";
 import NextAuth from "next-auth";
+export const dynamic = "force-dynamic"; // Ensure no prerendering
 
 const handler=NextAuth(NEXT_AUTH_CONFIG);
 
